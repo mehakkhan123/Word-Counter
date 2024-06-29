c7c6f2dc927c0558d616e4f64edaa7db864df311
Mehakkhan123 <mahekkhan284@gmail.com>
2024-06-29 17:08:29 +0500

    word counter

---
+++ index.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message: "Enter your setence to count words:",
+  },
+]);
+const word = answer.sentence.trim().split(" ");
+console.log(word);
+console.log(`Your word counts are ${word.length}.`);
